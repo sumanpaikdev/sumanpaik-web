0d202d2e7ea8a78da82c6dedf0e1be04fe726ead
Suman Paik <sumanpaik.dev@gmail.com>
2022-12-30 11:06:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <h1>this si my website
+      </h1>
+    </div>
+  )
+}
+
+export default App
